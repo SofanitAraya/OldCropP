9ea6f0e692e17d9cd46170c9584fe3850a94dc74
SofanitAraya <sofanitgirma.araya@adelaide.edu.au>
2016-02-25 17:34:18 +1030

    bbb

--- R/CropPhenology.R
+++ R/CropPhenology.R
@@ -131,6 +131,7 @@ PhenoMetrics<- function (RawPath, BolAOI){
   r=length(try[[1]][,"value"])
   
   Head="ID X-Cord Y_Cord T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13 T14 T15 T16 T17 T18 T19 T20 T21 T22 T23"
+  write(Head, file="All_Pixels.txt", append=TRUE, ncolumns=(length(Head))
   #write.xlsx(Head, file="All_Pixels.xls", append=TRUE, ncolumns=length(Head), sep=" ")
   
   while(s>0 & s<(r+1)){ #iterate through the each pixel
